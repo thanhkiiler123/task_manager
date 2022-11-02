521996f8c40548d95f33cf3419f24405601f9c99
Thanh Nguyen <nguyenlongthanhIT@gmail.com>
2022-11-02 09:36:20 +0700

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+require("dotenv").config();
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error");
-require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const mongo_uri = process.env.MONGO_URI;
 
 //middleware
 app.use(express.static("./public"));
 app.use(express.json());
+
 //routes
 app.use("/api/v1/tasks", tasks);
 
@@ -21,7 +22,9 @@ app.use(errorHandlerMiddleware);
 const start = async () => {
     try {
         await connectDB(mongo_uri);
-        app.listen(port, console.log(`server is listening on port: ${port}`));
+        app.listen(port, () =>
+            console.log(`server is listening on port: ${port}`),
+        );
     } catch (err) {
         console.log(err);
     }
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,13 @@
-const Task = require("../models/task");
+const Task = require("../models/Task");
 const asyncWrapper = require("../middleware/async");
 const { createCustomError } = require("../errors/custom-error");
 
-const getAllTasks = asyncWrapper(async (req, res, next) => {
+const getAllTasks = asyncWrapper(async (req, res) => {
     const tasks = await Task.find({});
     res.status(200).json({ tasks });
 });
 
-const createTask = asyncWrapper(async (req, res, next) => {
+const createTask = asyncWrapper(async (req, res) => {
     const task = await Task.create(req.body);
     res.status(200).json({ task });
 });
@@ -31,6 +31,7 @@ const updateTask = asyncWrapper(async (req, res, next) => {
     if (!task) {
         return next(createCustomError(`No task with id: ${taskID}`, 404));
     }
+    res.status(200).json({ task });
 });
 
 const deleteTask = asyncWrapper(async (req, res, next) => {
